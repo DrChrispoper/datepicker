97405248a97a00c4413186bc3ad90b1c471eee97
Christopher Hockley <105942@supinfo.com>
2020-11-12 14:03:41 +0000

    feat: :sparkles: Fetch 4 weeks of dates

--- src/containers/DatePicker.tsx
+++ src/containers/DatePicker.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import { StyleSheet, FlatList, View } from 'react-native';
+import moment from 'moment';
 import DateComponent from '../components/DateComponent';
-
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Day 1',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Day 2',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Day 3',
-  },
-];
+import { daysInAdvance } from '../utils/constants';
 
 export default function DatePicker() {
-  const renderItem = ({ item }) => <DateComponent title={item.title} />;
+  var now = moment();
+  let days = [];
+
+  for (let index = 0; index < daysInAdvance; index++) {
+    days.push(moment(now).add(index, 'days'));
+  }
+
+  const renderItem = ({ item }) => (
+    <DateComponent title={item.format('ddd DD')} />
+  );
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={DATA}
+        data={days}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
+        keyExtractor={item => `${item.dayOfYear()}`}
         horizontal={true}
       />
     </View>
